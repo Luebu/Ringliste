05908e047819103e1a03d57af304d52292e9036f
Akomi03 <55061661+Akomi03@users.noreply.github.com>
2020-11-09 19:57:47 +0100

    final

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	    Spiel g = new Spiel();
-		Scanner s = new Scanner(System.in);
-	    System.out.println("Wie viele?");
-	    int tmp=s.nextInt();
-	    if(tmp>1) {
-			g.erstellen(tmp);
-		}else{
-	    	System.out.println("Die Zahl muss größer als 2 sein");
-	    	return;
-		}
-	    System.out.println("Welche stelle?");
-	    Person p = g.game(s.nextInt());
-	    System.out.println("Das Alter der Person ist "+p.getAlter());
-	    System.out.println("Der Name der Person ist "+ p.getName());
+        Spiel g = new Spiel();
+        Scanner s = new Scanner(System.in);
+        System.out.println("Wie viele?");
+        int tmp = s.nextInt();
+        if (tmp > 1) {
+            g.erstellen(tmp);
+        } else {
+            System.out.println("Die Zahl muss größer als 2 sein");
+            return;
+        }
+        System.out.println("Welche stelle?");
+        Person p = g.game(s.nextInt());
+        System.out.println("Das Alter der Person ist " + p.getAlter());
+        System.out.println("Der Name der Person ist " + p.getName());
     }
 }
