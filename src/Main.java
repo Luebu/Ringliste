0bd21f0c53c8001fa03eba0c8519a39aeecc742f
Akomi03 <55061661+Akomi03@users.noreply.github.com>
2020-11-09 19:16:41 +0100

    final

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,13 @@ public class Main {
 	    Spiel g = new Spiel();
 		Scanner s = new Scanner(System.in);
 	    System.out.println("Wie viele?");
-	    g.erstellen(s.nextInt());
+	    int tmp=s.nextInt();
+	    if(tmp>1) {
+			g.erstellen(tmp);
+		}else{
+	    	System.out.println("Die Zahl muss größer als 2 sein");
+	    	return;
+		}
 	    System.out.println("Welche stelle?");
 	    System.out.println(g.game(s.nextInt()));
     }
